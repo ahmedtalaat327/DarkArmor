4ea6bf0aa60f4bf2e5736285a101208cb25b92d6
Ahmed Talaat <ahmedtm2015@gmail.com>
2024-08-06 06:00:06 +0300

    init adding 'ManARP' class 🚀🚀

---
+++ Data/ManARP.cs
@@ -0,0 +1,24 @@
+﻿using DarkArmor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarkArmor.Data
+{
+    public class ManARP
+    {
+        private string? url = null;
+        private int index = 0;
+        
+        private int spoofed = 0;
+        //we will get the gate fron this innstance
+        private NetworkDevice? sacrifiicedDevice = null;
+
+
+        public ManARP() { 
+        
+        }
+    }
+}
